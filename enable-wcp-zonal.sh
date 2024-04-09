3d2c5902be7495cfabeeb3324235ef12fcda5ef2
papivot <47264956+papivot@users.noreply.github.com>
2024-04-08 21:38:01 -0400

    Single consolidate setup

--- enable-wcp-zonal.sh
+++ enable-wcp-zonal.sh
@@ -1,34 +1,44 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ###################################################
 # Enter temp variables here
 ###################################################
 
 VCENTER_VERSION=8
-VCENTER_HOSTNAME=192.168.100.50
+VCENTER_HOSTNAME=10.0.0.12
 VCENTER_USERNAME=administrator@vsphere.local
-VCENTER_PASSWORD='VMware1!'
-
-export AVI_CLOUD='domain-c99'
-export AVI_HOSTNAME=192.168.100.58
-export AVI_USERNAME=admin
-export AVI_PASSWORD='VMware1!'
-
-# export NSX_INGRESS_CIDR='10.220.3.16'
-# export NSX_EGRESS_CIDR=10.220.30.80
-# export NSX_EDGE_CLUSTER_ID='f98c44c3-131b-4f3d-811f-95edc1b00b86'
-# export NSX_TO_GATEWAY='nsxt01-t0-tr'
-# export NSX_NAMESPACE_NETWORK='10.244.0.0'
-# export NSX_VDS='50 01 79 90 7e 9c 73 51-ad af 31 fa 15 49 8d e0'
+VCENTER_PASSWORD='VMware123!VMware123!'
+DEPLOYMENT_TYPE='NSX'
+#DEPLOYMENT_TYPE='VDS'
+export DNS_SERVER='10.0.0.221'
+export NTP_SERVER='10.0.0.221'
+export DNS_SEARCHDOMAIN='vcf.sddc.lab'
 
 K8S_SUP_ZONE1='zone1'
 K8S_SUP_ZONE2='zone2'
 K8S_SUP_ZONE3='zone3'
-K8S_CONTENT_LIBRARY=utkg
-K8S_STORAGE_POLICY=tanzu
-K8S_MGMT_PORTGROUP1='DVPG-Management-network-1'
-K8S_MGMT_PORTGROUP2='DVPG-Management-network-2'
-K8S_MGMT_PORTGROUP3='DVPG-Management-network-3'
+
+export MGMT_STARTING_IP='10.0.0.60'
+export MGMT_GATEWAY_CIDR='10.0.0.221/24'
+
+export AVI_CLOUD='domain-c99'
+export AVI_HOSTNAME='10.0.0.20'
+export AVI_USERNAME=admin
+export AVI_PASSWORD='VMware123!VMware123!'
+
+export NSX_EDGE_CLUSTER_ID='408bc83a-e05a-4da5-85cf-aeb4625981c4'
+export NSX_T0_GATEWAY='16110da9-eb11-4c0d-af33-437c9dc7ed3b'
+export NSX_DVS_PORTGROUP='mgmt-vds01'
+#### TO add these
+export NSX_INGRESS_CIDR='10.220.3.16/24'
+export NSX_EGRESS_CIDR='10.220.30.80/24'
+export NSX_NAMESPACE_NETWORK='10.244.0.0/20'
+
+K8S_CONTENT_LIBRARY=tkgs
+K8S_STORAGE_POLICY=tkgs
+K8S_MGMT_PORTGROUP1='sddc-vds01-mgmt'
+K8S_MGMT_PORTGROUP2='mgmt-domain-c02-vds-01-pg-mgmt'
+K8S_MGMT_PORTGROUP3='mgmt-domain-c03-vds-01-pg-mgmt'
 K8S_WKD0_PORTGROUP='Workload0-VDS-PG' #NOT NEEDED FOR NSX
 
 ###################################################
@@ -37,27 +47,35 @@ HEADER_CONTENTTYPE="Content-Type: application/json"
 
 content_library_json()
 {
-	cat <<EOF
+        cat <<EOF
 {
-	"name": "${K8S_CONTENT_LIBRARY}"
+        "name": "${K8S_CONTENT_LIBRARY}"
 }
 EOF
 }
 
 rm -f /tmp/temp_*.*
-cp sample-zonal-vds-80u3.json zonal.json
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
+then
+        cp sample-zonal-vds-80u3.json zonal.json
+else
+        cp sample-zonal-nsx-80u3.json zonal.json
+fi
 
 ################################################
 # Get NSXALB CA CERT
 ###############################################
-echo "Getting NSX ALB CA Certificate for  ${AVI_HOSTNAME} ..."
-openssl s_client -showcerts -connect ${AVI_HOSTNAME}:443  </dev/null 2>/dev/null|sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > /tmp/temp_avi-ca.cert
-if [ ! -s /tmp/temp_avi-ca.cert ]
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
 then
-    echo "Error: Could not connect to the NSX ALB endpoint. Please validate!!"
-    exit 1
+        echo "Getting NSX ALB CA Certificate for  ${AVI_HOSTNAME} ..."
+        openssl s_client -showcerts -connect ${AVI_HOSTNAME}:443  </dev/null 2>/dev/null|sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > /tmp/temp_avi-ca.cert
+        if [ ! -s /tmp/temp_avi-ca.cert ]
+        then
+                echo "Error: Could not connect to the NSX ALB endpoint. Please validate!!"
+                exit 1
+        fi
+        export AVI_CACERT=$(jq -sR . /tmp/temp_avi-ca.cert)
 fi
-export AVI_CACERT=$(jq -sR . /tmp/temp_avi-ca.cert)
 
 ################################################
 # Login to VCenter and get Session ID
@@ -65,8 +83,8 @@ export AVI_CACERT=$(jq -sR . /tmp/temp_avi-ca.cert)
 SESSION_ID=$(curl -sk -X POST https://${VCENTER_HOSTNAME}/rest/com/vmware/cis/session --user ${VCENTER_USERNAME}:${VCENTER_PASSWORD} |jq -r '.value')
 if [ -z "${SESSION_ID}" ]
 then
-	echo "Error: Could not connect to the VCenter. Please validate!!"
-	exit 1
+        echo "Error: Could not connect to the VCenter. Please validate!!"
+        exit 1
 fi
 echo Authenticated successfully to VC with Session ID - ${SESSION_ID} ...
 HEADER_SESSIONID="vmware-api-session-id: ${SESSION_ID}"
@@ -113,19 +131,19 @@ fi
 echo "Searching for Content Library ${K8S_CONTENT_LIBRARY} ..."
 response=$(curl -ks --write-out "%{http_code}" -X POST -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" -d "$(content_library_json)" https://${VCENTER_HOSTNAME}/api/content/library?action=find --output /tmp/temp_contentlib.json)
 if [[ "${response}" -ne 200 ]] ; then
-	echo "Error: Could not fetch content librarys. Please validate!!"
-	exit 1
+        echo "Error: Could not fetch content librarys. Please validate!!"
+        exit 1
 fi
 
 export TKGContentLibrary=$(jq -r '.[]' /tmp/temp_contentlib.json)
 if [ -z "${TKGContentLibrary}" ]
 then
-   	echo "Error: Could not fetch content library - ${K8S_CONTENT_LIBRARY} . Please validate!!"
-   	exit 1
+        echo "Error: Could not fetch content library - ${K8S_CONTENT_LIBRARY} . Please validate!!"
+        exit 1
 fi
 
 ################################################
-# Get stroage policy details from vCenter
+# Get storage policy details from vCenter
 ###############################################
 echo "Searching for Storage Policy ${K8S_STORAGE_POLICY} ..."
 response=$(curl -ks --write-out "%{http_code}" -X GET  -H "${HEADER_SESSIONID}" https://${VCENTER_HOSTNAME}/api/vcenter/storage/policies --output /tmp/temp_storagepolicies.json)
@@ -156,6 +174,7 @@ export TKGMgmtNetwork1=$(jq -r --arg K8S_MGMT_PORTGROUP1 "$K8S_MGMT_PORTGROUP1" 
 export TKGMgmtNetwork2=$(jq -r --arg K8S_MGMT_PORTGROUP2 "$K8S_MGMT_PORTGROUP2" '.[]| select(.name == $K8S_MGMT_PORTGROUP2)|.network' /tmp/temp_networkportgroups.json)
 export TKGMgmtNetwork3=$(jq -r --arg K8S_MGMT_PORTGROUP3 "$K8S_MGMT_PORTGROUP3" '.[]| select(.name == $K8S_MGMT_PORTGROUP3)|.network' /tmp/temp_networkportgroups.json)
 export TKGWorkload0Network=$(jq -r --arg K8S_WKD0_PORTGROUP "$K8S_WKD0_PORTGROUP" '.[]| select(.name == $K8S_WKD0_PORTGROUP)|.network' /tmp/temp_networkportgroups.json)
+
 if [ -z "${TKGMgmtNetwork1}" ]
 then
         echo "Error: Could not fetch portgroup - ${K8S_MGMT_PORTGROUP1} . Please validate!!"
@@ -171,19 +190,40 @@ then
         echo "Error: Could not fetch portgroup - ${K8S_MGMT_PORTGROUP3} . Please validate!!"
         exit 1
 fi
-if [ -z "${TKGWorkload0Network}" ]
+
+################################################
+# Get compatiable VDS switch from vCenter
+###############################################
+if [ ${DEPLOYMENT_TYPE} == "NSX" ]
 then
-        echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
-        exit 1
-fi
-if [ -z "${TKGWorkload1Network}" ]
-then
-        echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
-        exit 1
+        echo "Searching for NSX compatible VDS switch ..."
+        response=$(curl -ks --write-out "%{http_code}" -X POST  -H "${HEADER_SESSIONID}" https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/networks/nsx/distributed-switches?action=check_compatibility --output /tmp/temp_vds.json)
+        if [[ "${response}" -ne 200 ]] ; then
+                echo "Error: Could not fetch VDS details. Please validate!!"
+                exit 1
+        fi
+        export NSX_DVS=$(jq -r --arg NSX_DVS_PORTGROUP "$NSX_DVS_PORTGROUP" '.[]| select((.compatible==true) and .name == $NSX_DVS_PORTGROUP)|.distributed_switch' /tmp/temp_vds.json)
+        if [ -z "${NSX_DVS}" ]
+        then
+                echo "Error: Could not fetch NSX compatible VDS - ${NSX_DVS_PORTGROUP} . Please validate!!"
+                exit 1
+        fi
 fi
 
 ################################################
-# Get cluster details from vCenter
+# Get WORKLOAD Netwrok exist for VDS
+###############################################
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
+then
+        if [ -z "${TKGWorkload0Network}" ]
+        then
+                echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
+                exit 1
+        fi
+fi
+
+################################################
+# Enable supervisor
 ###############################################
 envsubst < zonal.json > temp_final.json
 
